c206e0b71868c511ac05397ebffcb082e87b32a7
drwadu <alamzan94@live.de>
2022-01-11 14:29:29 +0100

    update.

--- Main.hs
+++ Main.hs
@@ -9,8 +9,7 @@ main = do
   let nnf = lines contents
   let nc = readInt $ words (head nnf) !! 1
   let body = dotBody nc nnf
-  let nnfStack = stack nnf
-  -- print $ map (evalCount nnfStack) nnfStack
+
   putStr "digraph nnftd  {"
   mapM_ putStr body
   putStrLn "\n}"
@@ -21,36 +20,17 @@ toNodes dag = reverse $ zip (reverse $ tail dag) [1 ..]
 readInt :: String -> Integer
 readInt = read :: String -> Integer
 
-stack :: [[Char]] -> [[Integer]]
-stack = map gate . tail
-
-gate :: [Char] -> [Integer]
-gate s
-  | head s == 'L' = [readInt . last . words $ s]
-  | head s == 'A' = (map readInt . drop 2 . words $ s) ++ [1]
-  | head s == 'O' = (map readInt . drop 3 . words $ s) ++ [0]
-  | otherwise = error "invalid nnf."
-
-evalCount :: Integral a => [[a]] -> [a] -> [a]
-evalCount nnf node
-  | length node == 1 = 1 : node
-  | null node = 0 : node
-  | otherwise = case last node of
-    -- 1 -> product (map (\i -> evalCount nnf $ nnf !! fromIntegral i) $ init node) : node
-    -- 0 -> sum (map (\i -> evalCount nnf $ nnf !! fromIntegral i) $ init node) : node
-    _ -> error "unknown error."
-
 dot :: Show a => Integer -> ([Char], a) -> [[Char]]
 dot nc (node, id)
   | head node == 'L' = ["\n\tNode_" ++ show id ++ " [label=" ++ show (readInt . last . words $ node) ++ "]"]
+  | head node == '1' = ["\n\tNode_" ++ show id ++ " [label=" ++ show (readInt . last . words $ node) ++ "]"]
+  | head node == '0' = ["\n\tNode_" ++ show id ++ " [label=" ++ show (readInt . last . words $ node) ++ "]"]
   | head node == 'A' = reverse ["\n\tNode_" ++ show id ++ " [label=A]", edges nc node id]
   | head node == 'O' = reverse ["\n\tNode_" ++ show id ++ " [label=O]", edges nc node id]
-  | otherwise = error "invalid nnf."
-
-dotWIthMappings :: Show a => Integer -> ([Char], a) -> [[Char]]
-dotWIthMappings = undefined
-
-countingGraph = undefined
+  | head node == '*' = reverse ["\n\tNode_" ++ show id ++ " [label=\"*\"]", edges nc node id]
+  | head node == '+' = reverse ["\n\tNode_" ++ show id ++ " [label=\"+\"]", edges nc node id]
+  -- | head node `elem` ['A', 'O', '+', '*'] = reverse ["\n\tNode_" ++ show id ++ " [label=" ++ show (head node) ++ "]", edges nc node id]
+  | otherwise = error "invalid file."
 
 edges :: Show a => Integer -> [Char] -> a -> [Char]
 edges n node id
@@ -62,6 +42,15 @@ edges n node id
     concatMap
       ((++) "\n" . ((++) (concat ["\tNode_", show id, " -> Node_"]) . show . abs . (-) n . readInt))
       (drop 3 $ words node)
+  | head node == '*' =
+    concatMap
+      ((++) "\n" . ((++) (concat ["\tNode_", show id, " -> Node_"]) . show . abs . (-) n . readInt))
+      (drop 2 $ words node)
+  | head node == '+' =
+    concatMap
+      ((++) "\n" . ((++) (concat ["\tNode_", show id, " -> Node_"]) . show . abs . (-) n . readInt))
+      (drop 2 $ words node)
+  | otherwise = error "invalid file."
 
 dotBody :: Integer -> [[Char]] -> [[Char]]
 dotBody nc nnf = reverse $ concatMap (dot nc) $ toNodes nnf
